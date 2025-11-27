40d41cae49caac4780aff2743a4df8d34844eb5c
박진현 <wlsgus866@gmail.com>
2025-11-27 14:55:04 +0900

    Fix: 초기 설정 수정

---
+++ src/main/java/com/example/elicesecondproject/mall/product/domain/ProductOption.java
@@ -0,0 +1,17 @@
+package com.example.elicesecondproject.mall.product.domain;
+
+
+
+import com.example.elicesecondproject.mall.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ProductOption extends BaseTimeEntity {
+
+}
