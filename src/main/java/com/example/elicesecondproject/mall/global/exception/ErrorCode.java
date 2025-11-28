d43b78038d2ab308925ad7376ddfffee8c2b0417
whddnjs039@naver.com <whddnjs039@naver.com>
2025-11-28 16:34:45 +0900

    chore: merge 충돌 해결

--- src/main/java/com/example/elicesecondproject/mall/global/exception/ErrorCode.java
+++ src/main/java/com/example/elicesecondproject/mall/global/exception/ErrorCode.java
@@ -25,6 +25,7 @@ public enum ErrorCode {
 
     // 409 Conflict
     DUPLICATE_RESOURCE(HttpStatus.CONFLICT, "C003", "이미 존재하는 데이터입니다."),
+    NOT_ENOUGH_STOCK(HttpStatus.CONFLICT, "I001", "재고가 부족합니다."),
 
     // 500 Internal Server Error
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S001", "서버 내부 오류가 발생했습니다.");
