d8c25dbc463444ef005b2d4cf9e4169157b03b43
박진현 <wlsgus866@gmail.com>
2025-12-02 00:08:01 +0900

    feat/PROD-F-02 완성

--- src/main/java/com/example/elicesecondproject/mall/domain/product/dto/ProductSummaryDto.java
+++ src/main/java/com/example/elicesecondproject/mall/domain/product/dto/ProductSummaryDto.java
@@ -1,7 +1,6 @@
 package com.example.elicesecondproject.mall.domain.product.dto;
 
 import com.example.elicesecondproject.mall.domain.product.entity.ProductStatus;
-import com.example.elicesecondproject.mall.domain.product.entity.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,21 +21,4 @@ public class ProductSummaryDto {
     private Double averageRating;
     private int reviewCount;
     private int WishListCount;
-
-
-    public static ProductSummaryDto from(Product product) {
-        return ProductSummaryDto.builder()
-                .id(product.getId())
-                .name(product.getName())
-                .price(product.getPrice())
-                .salePrice(product.getSalePrice())
-                .discountRate(product.getDiscountRate())
-                .status(product.getStatus())
-                .mainImageUrl(product.getMainImageUrl())
-                .averageRating(product.getAverageRating())
-                .reviewCount(product.getReviewCount())
-                .WishListCount(product.getWishListCount())
-                .build();
-    }
-
 }
