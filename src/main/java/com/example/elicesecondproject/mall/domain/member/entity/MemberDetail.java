80391bfefe2706da56acc4d15b7523c85c5d0b82
whddnjs039@naver.com <whddnjs039@naver.com>
2025-12-03 12:19:36 +0900

    refactor: 어드민 관련 시큐리티 수정 추가

--- src/main/java/com/example/elicesecondproject/mall/domain/member/entity/MemberDetail.java
+++ src/main/java/com/example/elicesecondproject/mall/domain/member/entity/MemberDetail.java
@@ -17,7 +17,7 @@ public class MemberDetail implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + member.getRole().name())); // TODO: 이거는 로그인에서 수정
+        return List.of(new SimpleGrantedAuthority("ROLE_" + member.getRole().name()));
     }
 
     @Override
