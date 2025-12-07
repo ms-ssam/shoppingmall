c1291f43fadcedf8ea945bb14b2724dddddaa036
[CLOUD6] 노푸름 <b1ue8o8@naver.com>
2025-12-07 20:27:46 +0900

    [PRODWD-UI-01] feat: 회원 탈퇴 화면 구현

--- src/main/java/com/example/elicesecondproject/mall/domain/member/dto/request/WithdrawMemberRequest.java
+++ src/main/java/com/example/elicesecondproject/mall/domain/member/dto/request/WithdrawMemberRequest.java
@@ -3,8 +3,9 @@ package com.example.elicesecondproject.mall.domain.member.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Getter
+@Getter @Setter
 @NoArgsConstructor
 public class WithdrawMemberRequest {
     @NotBlank(message = "비밀번호를 입력해주세요.")
