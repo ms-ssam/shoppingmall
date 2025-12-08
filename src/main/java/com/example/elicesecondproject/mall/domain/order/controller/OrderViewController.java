4aaf4919d36532487a8e4064f621cdfeb2151b69
whddnjs039@naver.com <whddnjs039@naver.com>
2025-12-08 20:58:03 +0900

    feat(order): service, controller, repository 추가

---
+++ src/main/java/com/example/elicesecondproject/mall/domain/order/controller/OrderViewController.java
@@ -0,0 +1,9 @@
+package com.example.elicesecondproject.mall.domain.order.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/order")
+public class OrderViewController {
+}
---
+++ src/main/java/com/example/elicesecondproject/mall/domain/order/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.example.elicesecondproject.mall.domain.order.repository;
+
+import com.example.elicesecondproject.mall.domain.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}
