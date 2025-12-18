4d7312e3a1aef743edfefc10de60479eac2f9ced
jinhyeon <wlsgus866@gmail.com>
2025-12-18 10:15:12 +0900

    fix: 임시저장

--- src/main/java/com/example/elicesecondproject/mall/domain/product/repository/ProductRepositoryCustomImpl.java
+++ src/main/java/com/example/elicesecondproject/mall/domain/product/repository/ProductRepositoryCustomImpl.java
@@ -158,7 +158,7 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
                 calculateSalePrice(),
                 product.discountRate,
                 product.status,
-                product.thumbnailUrl,
+                selectMainImage(),
                 product.averageRating,
                 product.reviewCount,
                 product.wishListCount,
@@ -176,7 +176,18 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
                 .as(SALE_PRICE_ALIAS);
     }
 
-
+    private Expression<String> selectMainImage() {
+        return JPAExpressions
+                .select(productImage.imageUrl)
+                .from(productImage)
+                .where(
+                        productImage.product.eq(product),
+                        productImage.imageType.eq(ImageType.MAIN),
+                        productImage.deletedAt.isNull()
+                )
+                .orderBy(productImage.displayOrder.asc())
+                .limit(1);
+    }
 
     private BooleanExpression productNotDeleted() {
         return product.deletedAt.isNull()
